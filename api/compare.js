ea41409529cc6b4d3b96d72885a35c6311482276
najemsaadeh <najemanjem80@gmail.com>
2025-06-07 18:30:09 +0300

    Update compare.js

--- api/compare.js
+++ api/compare.js
@@ -1,18 +1,25 @@
 export default async function handler(req, res) {
-  const path = req.query.path;
-  const url = `https://mobile-phone-specs-database.p.rapidapi.com/${path}`;
+  const { phone1, phone2 } = req.query;
+
+  if (!phone1 || !phone2) {
+    return res.status(400).json({ error: 'يجب تحديد كل من phone1 و phone2' });
+  }
+
+  const baseURL = 'https://mobile-phone-specs-database.p.rapidapi.com/phones/';
+  const headers = {
+    'X-RapidAPI-Key': '43a7e199bbmsh352e9bc0c3acaaap15259ejsn269a8ac851e8',
+    'X-RapidAPI-Host': 'mobile-phone-specs-database.p.rapidapi.com',
+  };
 
   try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '43a7e199bbmsh352e9bc0c3acaaap15259ejsn269a8ac851e8',
-        'X-RapidAPI-Host': 'mobile-phone-specs-database.p.rapidapi.com',
-      }
-    });
+    const [res1, res2] = await Promise.all([
+      fetch(baseURL + phone1, { headers }),
+      fetch(baseURL + phone2, { headers }),
+    ]);
 
-    const data = await response.json();
-    res.status(200).json(data);
+    const [data1, data2] = await Promise.all([res1.json(), res2.json()]);
+
+    res.status(200).json({ phone1: data1, phone2: data2 });
   } catch (err) {
     res.status(500).json({ error: 'حدث خطأ أثناء جلب البيانات', details: err.message });
   }
